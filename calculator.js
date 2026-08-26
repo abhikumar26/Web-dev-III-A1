225390bb7a958183faec32eb11731bb42a6318db
Abhinav <abhinavkumarprithvi7@gmail.com>
2026-08-26 13:05:46 +0530

    calculator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+const args = process.argv.slice(2);
+const operation = args[0]
+const num1 = parseFloat(args[1])
+const num2 = parseFloat(args[2])
+
+if (operation == "add"){
+    console.log(`Result- ${num1+num2}`);
+} else if (operation == "Subtract"){
+    console.log(`Result- ${num1-num2}`);
+} else if (operation == "Multiply"){
+    console.log(`Result- ${num1*num2}`);
+} else if (operation == "Divide"){
+    console.log(`Result- ${num1/num2}`);
+} else{
+    console.log("Invalid opration Performed...! PLEASE TRY AGAIN");
+    
+}
