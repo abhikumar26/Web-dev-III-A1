35b5eac441bd4f433cb5b94cc75317e9ca660566
Abhinav <abhinavkumarprithvi7@gmail.com>
2026-08-28 20:28:07 +0530

    changes

---
+++ server.js
@@ -0,0 +1,18 @@
+const { log } = require("console");
+const http = require("http");
+
+const server = http.createServer((req,res)=>{
+    if(req.url == "/"){
+        res.end("Welcome to node_server")
+    } else if(req.url == "/about"){
+        res.end("About Page")
+    } else if (req.url == "/contact"){
+        res.end("Contact-Page")
+    } else {
+        res.statusCode = 404;
+        res.end("404 error : Page not found")
+    }
+})
+server.listen(3000,()=>{
+    console.log("Server is running on Port at http//llocalhost 3000...!");
+})
